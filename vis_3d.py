804eb0eba84d49af607d8a2189ead64afb540c24
Francesco Stablum <stablum@gmail.com>
2014-11-01 23:54:28 +0100

    legend

--- vis_3d.py
+++ vis_3d.py
@@ -4,6 +4,7 @@ err,warn,info = __import__('log').setup("vis_3d")
 import numpy as np
 import pylab
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 from mpl_toolkits.mplot3d import Axes3D
 
@@ -11,7 +12,7 @@ def normalize(m):
     _min = np.min(m)
     _max = np.max(m)
     ret = (m - _min)/(_max - _min)
-    return ret
+    return ret,_min,_max
 
 def show(df):
     info("show")
@@ -25,8 +26,17 @@ def show(df):
     zs = df.weigth
     s = np.power(df.cylinders,2)
     cmap=plt.get_cmap('gnuplot2')
-    c = cmap(normalize(df.MPG))
-
-    ax.scatter(xs, ys, zs, s=s,c=c)
+    MPG_norm,MPG_min,MPG_max = normalize(df.MPG)
+    c = cmap(MPG_norm)
+    scatter = ax.scatter(xs, ys, zs, s=s,c=c)
+    info("scatter:")
+    info(scatter)
+    #import pdb;pdb.set_trace();
+    mappable = pylab.cm.ScalarMappable(cmap=cmap)
+    mappable.set_array(np.array([MPG_min,MPG_max]))
+    cbar = plt.colorbar(mappable,ax=ax)
+    cbar.set_label("MPG")
+    c = plt.Line2D(range(1), range(1), color="white", marker='o', markerfacecolor="white")
+    ax.legend([c],['marker size: cylinders'])
     info("end.")
     pylab.show()
